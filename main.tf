94d596c8a56fe8368b2740527407d9c68481e2ec
Jason McCoy <57716852+jmccoydev@users.noreply.github.com>
2021-11-08 20:39:31 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    venafi = {
-      source = "Venafi/venafi"
-      version = "0.13.0"
-    }
-  }
-}
-
 provider "venafi" {
     url          = "https://uvo1yjukjquyf5nqj8p.env.cloudshare.com/"
     trust_bundle = var.trust
@@ -29,13 +20,66 @@ resource "venafi_certificate" "webserver" {
     }
 }
 
-resource "venafi_ssh_certificate" "ssh_cert" {
-    key_id = "sshcertificate"
-    template = "SampleSSHCA"
-    valid_hours = 24
+resource "venafi_certificate" "appserver" {
+    common_name = "app.venafi.example"
+    san_dns = [
+        "app01.venafi.example",
+        "app02.venafi.example"
+    ]
+    algorithm = "RSA"
+    rsa_bits = "2048"
+    key_password = "123456"
+    custom_fields = {
+        "Cost Center" = "AB1234",
+        "Environment" = "Staging"
+    }
 }
 
-output "cert_certificate_dev" {
+resource "venafi_certificate" "dbserver" {
+    common_name = "db.venafi.example"
+    san_dns = [
+        "db01.venafi.example",
+        "db02.venafi.example"
+    ]
+    algorithm = "RSA"
+    rsa_bits = "2048"
+    key_password = "123456"
+    custom_fields = {
+        "Cost Center" = "AB1234",
+        "Environment" = "Staging"
+    }
+}
+
+resource "venafi_certificate" "loadbalancer" {
+    common_name = "loadbalancer.venafi.example"
+    san_dns = [
+        "lb01.venafi.example",
+        "lb02.venafi.example"
+    ]
+    algorithm = "RSA"
+    rsa_bits = "2048"
+    key_password = "123456"
+    custom_fields = {
+        "Cost Center" = "AB1234",
+        "Environment" = "Staging"
+    }
+}
+output "cert_certificate_web_dev" {
   value = venafi_certificate.webserver.certificate
   description = "The certificate of the web server"
+}
+
+output "cert_certificate_app_dev" {
+  value = venafi_certificate.appserver.certificate
+  description = "The certificate of the app server"
+}
+
+output "cert_certificate_db_dev" {
+  value = venafi_certificate.dbserver.certificate
+  description = "The certificate of the database server"
+}
+
+output "cert_certificate_loadbalancer_dev" {
+  value = venafi_certificate.loadbalancer.certificate
+  description = "The certificate of the loadbalancer"
 }
